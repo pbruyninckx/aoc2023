6b605182400ac84ffbc4cb585ab8e7be58e4d8f6
Pieter Bruyninckx <pbruyninckx@gmail.com>
2023-12-09 00:58:48 +0100

    Day 5 part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -3,10 +3,21 @@ use aoc2023::parse_numbers;
 use std::fs;
 use std::path::Path;
 
+#[derive(Clone, Debug)]
+struct Range {
+    start: i64,
+    length: i64,
+}
+
 struct MapRange {
     dest_start: i64,
     source_start: i64,
     length: i64,
+}
+
+struct MappedRange {
+    unmapped: Vec<Range>,
+    mapped: Option<Range>,
 }
 
 impl MapRange {
@@ -30,6 +41,40 @@ impl MapRange {
             None
         }
     }
+
+    fn apply_range(&self, range: &Range) -> MappedRange {
+        let range_end = range.start + range.length;
+        let source_end = self.source_start + self.length;
+        let dest_end = self.dest_start + self.length;
+
+        if range_end <= self.source_start || range.start >= source_end {
+            MappedRange {
+                mapped: None,
+                unmapped: vec![range.clone()],
+            }
+        } else {
+            let mut unmapped = vec![];
+            if range.start < self.source_start {
+                unmapped.push(Range {
+                    start: range.start,
+                    length: self.source_start - range.start,
+                });
+            }
+            if range_end > source_end {
+                unmapped.push(Range {
+                    start: source_end,
+                    length: range_end - source_end,
+                })
+            }
+            let mapped_start = self.apply(range.start).unwrap_or(self.dest_start);
+            let mapped_end = self.apply(range_end).unwrap_or(dest_end);
+            let mapped = Some(Range {
+                start: mapped_start,
+                length: mapped_end - mapped_start,
+            });
+            MappedRange { mapped, unmapped }
+        }
+    }
 }
 
 struct Mapping {
@@ -48,11 +93,36 @@ impl Mapping {
     }
 
     fn apply(&self, number: i64) -> i64 {
-        self.ranges
+        let ret = self
+            .ranges
             .iter()
             .filter_map(|range| range.apply(number))
             .next()
-            .unwrap_or(number)
+            .unwrap_or(number);
+        ret
+    }
+
+    fn apply_range(&self, range: &Range) -> Vec<Range> {
+        let mut ret = vec![];
+        let mut to_map = vec![range.clone()];
+        for map_range in &self.ranges {
+            let mut to_map_next: Vec<Range> = vec![];
+            for range_to_map in &to_map {
+                let mut half_mapped = map_range.apply_range(range_to_map);
+                to_map_next.append(&mut half_mapped.unmapped);
+                if let Some(mapped) = half_mapped.mapped {
+                    ret.push(mapped);
+                }
+            }
+            to_map = to_map_next;
+        }
+
+        ret.append(&mut to_map);
+        ret
+    }
+
+    fn apply_ranges(&self, ranges: &[Range]) -> Vec<Range> {
+        ranges.iter().flat_map(|r| self.apply_range(r)).collect()
     }
 }
 struct Almanac {
@@ -77,6 +147,17 @@ impl Almanac {
         }
         Ok(Self { seeds, maps })
     }
+
+    fn seed_ranges(&self) -> Vec<Range> {
+        let mut ret = vec![];
+        let mut it = self.seeds.iter().peekable();
+        while it.peek().is_some() {
+            let start = *it.next().unwrap();
+            let length = *it.next().unwrap();
+            ret.push(Range { start, length });
+        }
+        ret
+    }
 }
 
 fn solve(almanac: &Almanac) -> i64 {
@@ -93,6 +174,19 @@ fn solve(almanac: &Almanac) -> i64 {
         .unwrap()
 }
 
+fn solve2(almanac: &Almanac) -> i64 {
+    almanac
+        .maps
+        .iter()
+        .fold(almanac.seed_ranges(), |ranges, mapping| {
+            mapping.apply_ranges(&ranges)
+        })
+        .iter()
+        .map(|r| r.start)
+        .min()
+        .unwrap()
+}
+
 fn main() -> Result<(), Error> {
     let almanac = Almanac::from_lines(
         &fs::read_to_string(Path::new("data/input05.txt"))?
@@ -100,5 +194,6 @@ fn main() -> Result<(), Error> {
             .collect::<Vec<_>>(),
     )?;
     println!("{}", solve(&almanac));
+    println!("{}", solve2(&almanac));
     Ok(())
 }
