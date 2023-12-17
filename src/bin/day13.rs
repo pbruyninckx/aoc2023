9bc67252702cce280a7481f3ec3103a11eb4101b
Pieter Bruyninckx <pbruyninckx@gmail.com>
2023-12-17 18:01:47 +0100

    Day 13 part 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -26,10 +26,19 @@ fn parse_input(input: &str) -> Vec<Pattern> {
     input.split("\n\n").map(Pattern::from_str).collect()
 }
 
-fn mirror_index(lines: &Vec<String>) -> Option<usize> {
+fn count_diffs(a: &str, b: &str) -> usize {
+    zip(a.chars(), b.chars())
+        .map(|(ac, bc)| if ac == bc { 0 } else { 1 })
+        .sum()
+}
+
+fn mirror_index(lines: &Vec<String>, num_diffs: usize) -> Option<usize> {
     for i in 0..lines.len() - 1 {
-        if lines[i] == lines[i + 1]
-            && zip(lines[0..i].iter().rev(), lines[i + 2..].iter()).all(|(lhs, rhs)| lhs == rhs)
+        if count_diffs(&lines[i], &lines[i + 1]) <= num_diffs
+            && zip(lines[0..i + 1].iter().rev(), lines[i + 1..].iter())
+                .map(|(lhs, rhs)| count_diffs(lhs, rhs))
+                .sum::<usize>()
+                == num_diffs
         {
             return Some(i + 1);
         }
@@ -37,15 +46,19 @@ fn mirror_index(lines: &Vec<String>) -> Option<usize> {
     None
 }
 
-fn solve(input: &[Pattern]) -> usize {
+fn solve(input: &[Pattern], num_diffs: usize) -> usize {
     input
         .iter()
-        .map(|p| mirror_index(&p.cols).unwrap_or_else(|| mirror_index(&p.rows).unwrap() * 100))
+        .map(|p| {
+            mirror_index(&p.cols, num_diffs)
+                .unwrap_or_else(|| mirror_index(&p.rows, num_diffs).unwrap() * 100)
+        })
         .sum()
 }
 
 fn main() -> Result<(), Error> {
     let input = parse_input(&fs::read_to_string(Path::new("data/input13.txt"))?);
-    println!("{}", solve(&input));
+    println!("{}", solve(&input, 0));
+    println!("{}", solve(&input, 1));
     Ok(())
 }
