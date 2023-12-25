07c8e349c68d97d04920b4ab0dd6cc4bad75b003
Pieter Bruyninckx <pbruyninckx@gmail.com>
2023-12-26 00:06:35 +0100

    Day 19 part 2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,8 +1,43 @@
 use lazy_static::lazy_static;
+use num::clamp;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+
+#[derive(Clone, Copy)]
+struct Range {
+    begin: i64,
+    end: i64,
+}
+
+impl Range {
+    fn split(&self, value: i64, comp: &str) -> (Self, Self) {
+        match comp {
+            "<" => (
+                Range {
+                    begin: self.begin,
+                    end: clamp(value, self.begin, self.end),
+                },
+                Range {
+                    begin: clamp(value, self.begin, self.end),
+                    end: self.end,
+                },
+            ),
+            ">" => (
+                Range {
+                    begin: clamp(value + 1, self.begin, self.end),
+                    end: self.end,
+                },
+                Range {
+                    begin: self.begin,
+                    end: clamp(value + 1, self.begin, self.end),
+                },
+            ),
+            _ => panic!("Invalid input"),
+        }
+    }
+}
 
 struct Part {
     x: i64,
@@ -60,9 +95,16 @@ impl Target {
     }
 }
 
+struct ConditionParams {
+    var: String,
+    comp: String,
+    value: i64,
+}
+
 struct Rule {
     condition: Box<dyn Fn(&Part) -> bool>,
     target: Target,
+    condition_params: Option<ConditionParams>,
 }
 
 impl Rule {
@@ -71,8 +113,10 @@ impl Rule {
             static ref RE: Regex = Regex::new("([xmas])(.)([0-9]+):(.+)").unwrap();
         }
         if let Some(captures) = RE.captures(string) {
-            let selector_fn = Part::selector_func(captures.get(1).unwrap().as_str());
-            let comp_fn = match captures.get(2).unwrap().as_str() {
+            let var = captures.get(1).unwrap().as_str();
+            let selector_fn = Part::selector_func(var);
+            let comp = captures.get(2).unwrap().as_str();
+            let comp_fn = match comp {
                 "<" => i64::lt,
                 ">" => i64::gt,
                 _ => panic!("Invalid input"),
@@ -80,11 +124,21 @@ impl Rule {
             let value = captures.get(3).unwrap().as_str().parse::<i64>().unwrap();
             let condition = Box::new(move |part: &Part| comp_fn(&selector_fn(part), &value));
             let target = Target::from_str(captures.get(4).unwrap().as_str());
-            Rule { condition, target }
+            let condition_params = Some(ConditionParams {
+                var: String::from(var),
+                comp: String::from(comp),
+                value,
+            });
+            Rule {
+                condition,
+                target,
+                condition_params,
+            }
         } else {
             Rule {
                 condition: Box::new(|_| true),
                 target: Target::from_str(string),
+                condition_params: None,
             }
         }
     }
@@ -105,10 +159,57 @@ impl Workflow {
     }
 }
 
+type PartRange = HashMap<String, Range>;
+
+trait PartRangeExt {
+    fn split(&self, condition: &ConditionParams) -> (PartRange, PartRange);
+    fn new_xmas() -> Self;
+    fn num_combinations(&self) -> i64;
+}
+
+impl PartRangeExt for PartRange {
+    fn split(&self, condition: &ConditionParams) -> (PartRange, PartRange) {
+        let mut ret1 = self.clone();
+        let mut ret2 = self.clone();
+
+        let (range1, range2) = self
+            .get(&condition.var)
+            .unwrap()
+            .split(condition.value, &condition.comp);
+        ret1.entry(condition.var.clone())
+            .and_modify(move |r| *r = range1);
+        ret2.entry(condition.var.clone())
+            .and_modify(move |r| *r = range2);
+
+        (ret1, ret2)
+    }
+
+    fn new_xmas() -> Self {
+        "xmas"
+            .chars()
+            .map(|c| {
+                (
+                    String::from(c),
+                    Range {
+                        begin: 1,
+                        end: 4001,
+                    },
+                )
+            })
+            .collect()
+    }
+
+    fn num_combinations(&self) -> i64 {
+        self.values().map(|r| r.end - r.begin).product()
+    }
+}
+
 type System = HashMap<String, Workflow>;
 
 trait SystemExt {
     fn accept(&self, part: &Part) -> bool;
+
+    fn num_combinations(&self, rule: &str, part_range: &PartRange) -> i64;
 }
 
 impl SystemExt for System {
@@ -127,6 +228,32 @@ impl SystemExt for System {
                 }
             }
         }
+    }
+
+    fn num_combinations(&self, rule: &str, part_range: &PartRange) -> i64 {
+        let rules = &self.get(rule).unwrap().rules;
+        let mut ret = 0;
+
+        let mut part_range = part_range.clone();
+        for rule in rules {
+            if let Some(condition_parameters) = &rule.condition_params {
+                let (this_part_range, next_part_range) = part_range.split(condition_parameters);
+                part_range = next_part_range;
+                ret += match &rule.target {
+                    Target::Workflow(workflow) => self.num_combinations(workflow, &this_part_range),
+                    Target::Accepted => this_part_range.num_combinations(),
+                    Target::Rejected => 0,
+                }
+            } else {
+                ret += match &rule.target {
+                    Target::Workflow(workflow) => self.num_combinations(workflow, &part_range),
+                    Target::Accepted => part_range.num_combinations(),
+                    Target::Rejected => 0,
+                }
+            }
+        }
+
+        ret
     }
 }
 
@@ -165,8 +292,13 @@ fn solve(input: &(System, Vec<Part>)) -> i64 {
         .sum()
 }
 
+fn solve2(system: &System) -> i64 {
+    system.num_combinations("in", &PartRange::new_xmas())
+}
+
 fn main() {
     let input = parse_input(&fs::read_to_string(Path::new("data/input19.txt")).unwrap());
 
     println!("{}", solve(&input));
+    println!("{}", solve2(&input.0));
 }
